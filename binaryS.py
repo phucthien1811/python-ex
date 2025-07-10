0e8f84c094bb81690cc376db2d093e17712b30ad
Thien Tiy <jack0baohbocon@gmail.com>
2025-07-10 20:49:33 +0700

    fibonacy

--- binaryS.py
+++ binaryS.py
@@ -3,12 +3,12 @@ def binary_search(arr, x):
     while left <= right:
         mid = (left + right) // 2
         if arr[mid] == x:
-            return mid  # Tìm thấy
+            return mid 
         elif arr[mid] < x:
             left = mid + 1
         else:
             right = mid - 1
-    return -1  # Không tìm thấy
+    return -1  
 
 arr = [1, 3, 5, 7, 9, 11]
-print(binary_search(arr, 7))  # Kết quả: 3
+print(binary_search(arr, 7)) 
