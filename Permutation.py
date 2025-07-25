4f6b42165ebb7ec4ad80310c3c2d52ade324d9da
Thien Tiy <jack0baohbocon@gmail.com>
2025-07-25 22:42:18 +0700

    tổ hợp - sinh tất cả hoán vịvị

---
+++ Permutation.py
@@ -0,0 +1,17 @@
+def permute(nums):
+    res = []
+    def backtrack(path, used):
+        if len(path) == len(nums):
+            res.append(path[:])
+            return
+        for i in range(len(nums)):
+            if used[i]: continue
+            used[i] = True
+            path.append(nums[i])
+            backtrack(path, used)
+            path.pop()
+            used[i] = False
+    backtrack([], [False]*len(nums))
+    return res
+
+print(permute([1,2,3]))
