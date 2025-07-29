ff09a17f57137bb628a1e8b96b49ee2a78b5273a
Thien Tiy <jack0baohbocon@gmail.com>
2025-07-29 20:52:50 +0700

    clear

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-# Ghi vào file
-with open('kkk.py', 'w') as file:
-    file.write('phuc thien dep trai')
-
-# Đọc lại file vừa ghi
-with open('kkk.py', 'r') as file:
-    data = file.read()
-
-print(data)  # In ra nội dung đã ghi
-file.close()
-
-print(file.closed)
+hhh
