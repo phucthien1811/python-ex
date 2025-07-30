363c75a3662b15da7ac46ba9d0551050b5a12ffb
Thien Tiy <jack0baohbocon@gmail.com>
2025-07-30 21:00:01 +0700

    test 2

--- test.py
+++ test.py
@@ -2,19 +2,14 @@ class Foo:
     name = 'Foo'
     def getName(self):
         print("Class: Foo")
+        
 class Bar(Foo):
     name = 'Bar'
     def getName(self):
-        print("Class: Bar")
+        print("Atribute name = " + super().name)
+        super().getName()
 
-print(Foo().name)
-Foo().getName()
-print(Bar().name)
 Bar().getName()
-
 # Ket qua:
-# Foo
+# Atribute name = Foo
 # Class: Foo
-#
-# Bar
-# Class: Bar
