d20f47e2af2e7afa65881acf89837c8bd560f60b
Thien Tiy <jack0baohbocon@gmail.com>
2025-07-30 21:23:21 +0700

    âdh test kế thauwf

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 class First:
     def getClass(self):
         print("Class Fist")
+        super().getClass()
 
 class Second:
     def getClass(self):
@@ -15,3 +16,4 @@ third.getClass()
 
 # Kết Quả:
 # Class Fist
+# Class Second
