6f5e774d5f77a230cdadda9d0368902cb233fc55
Thien Tiy <jack0baohbocon@gmail.com>
2025-07-30 20:26:12 +0700

    ssh

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-hhh
+class Foo:
+    name = 'Foo'
+    def getName(self):
+        print("Class: Foo")
+class Bar(Foo):
+    name = 'Bar'
+    def getName(self):
+        print("Class: Bar")
+
+print(Foo().name)
+Foo().getName()
+print(Bar().name)
+Bar().getName()
+
+# Ket qua:
+# Foo
+# Class: Foo
+#
+# Bar
+# Class: Bar
