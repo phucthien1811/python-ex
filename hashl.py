fd7d00dd154a2a88dc59a67b89be5f18af62a791
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-07 21:33:25 +0700

    new

--- hashl.py
+++ hashl.py
@@ -13,8 +13,4 @@ print(rolling_hash("hell") == rolling_hash("hell"))  # True
 # Đây là pseudocode ngắn gọn, có thể dùng thư viện hoặc viết lại rõ hơn nếu cần
 
 # pip install munkres
-from munkres import Munkres
-m = Munkres()
-matrix = [[5, 9, 1], [10, 3, 2], [8, 7, 4]]
-indexes = m.compute(matrix)
-print("Chi phí nhỏ nhất:", sum(matrix[row][column] for row, column in indexes))
+
