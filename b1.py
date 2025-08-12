86308e04d12dabf33b7869ab96a9821926f36a9c
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-12 20:45:36 +0700

    bài 2 vidu

--- b1.py
+++ b1.py
@@ -1,10 +1,6 @@
-#test for student
-try: #khối lệnh có thể xảy ra lỗi 
-   print('nhập vào a')
-   a = int(input())
-   print('nhập vào b')
-   b = int(input())
-   c = a + b 
-   print('Tổng của a và b là:', c)
-except: # khối lệnh đcc thực thi khi lỗi xảy ra .
-    print('Đã xảy ra lỗi khi nhập dữ liệu. Vui lòng thử lại.')
+#Nhap gia tri {Ten cua ban}
+ten = input()
+
+#Xuat cau voi dinh dang theo yeu cau de bai
+#Tham so sep='--' giup ngan cach cac tu boi '--'
+print('Xin', 'chao!', 'Toi', 'ten', 'la', ten, sep='--')
