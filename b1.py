487dcd378556092df534d023b3c363d6c3f64a9b
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-14 20:25:43 +0700

    vidu 2

--- b1.py
+++ b1.py
@@ -1,8 +1,5 @@
-#Nhap so A tu ban phim va chuyen sang kieu so thuc
-soA = float(input())
+x = map(int, input().split())
 
-#Nhap so B tu ban phim va chuyen sang kieu so nguyen
-soB = int(input())
+y= sum(x)
+print('tổng của các số là:', y)
 
-#Dung ham format de dinh dang chuoi dau ra
-print('{0:.{1}f}'.format(soA, soB))
