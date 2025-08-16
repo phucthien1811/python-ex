09f421be6b577c9b617083e83aeb89e09f6beff4
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-16 13:27:36 +0700

    test 9

--- index.py
+++ index.py
@@ -2,17 +2,16 @@ with open("testfile.txt", "r") as f:
     lines = f.readlines()
 line1 = lines[0].strip()
 numbers = line1.split()
-nums = map(int, numbers)
-total = sum(nums)
+R = float(numbers[0])
+S = R * 3.14 
+C = 3.14 * pow(R, 2) 
 
 with open("output.txt", "w") as f1:
-    f1.write(f"The sum of the numbers is: {total}\n")
+    f1.write(f"chu vi: {S}\n")
+    f1.write(f"dien tich: {C}\n")
    
 
              
     
-   
-   
-    
-    
-
+f.close()
+f1.close()
