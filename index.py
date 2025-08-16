981994d7bdd2bfc65c0df7355fd5c6f26a6a731c
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-16 12:05:53 +0700

    test 7

--- index.py
+++ index.py
@@ -1,14 +1,22 @@
 with open("testfile.txt", "r") as f:
     lines = f.readlines()
 
-line1 = lines[0].strip()
-print(line1)   # lấy dòng 1
-print(type(line1))  # kiểm tra kiểu dữ liệu của dòng 1
-numbers = line1.split()  
-print(type(numbers))  # kiểm tra kiểu dữ liệu của numbers
-print(numbers)  # ???  bước này sẽ ra gì
-nums = list(map(int, numbers))
-print(nums)
-print(type(nums))  # ???  thử in ra xem
-total = sum(nums)          # ???  cuối cùng ta có gì
-print('tong cua minh la: ',total)
+n = lines[0].strip()
+
+n = int(n)  # Convert the first line to an integer
+
+if n < 0:
+    print('dữ liệu trong file ko hợp lệ')
+else :
+   for i in range(1,n+1):
+       numbers = lines[i].split()    # ???  bước này sẽ ra gì
+       a , b = map(int, numbers)  # ???  thử in ra xem
+       print(f'tích của hàng {i} là {a*b}')
+       
+             
+    
+   
+   
+    
+    
+
