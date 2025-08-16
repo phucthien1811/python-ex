da38ea7c111c9b7c33d1223f81e441ff2f45b81f
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-16 10:04:22 +0700

    test 5

--- index.py
+++ index.py
@@ -1,12 +1,16 @@
 f = open("Testfile.txt",'r')
 print('nhập vào dòng muốn đọc: ')
 x = int(input())
-
-
 lines = f.readlines()
+i =0 
+while i < x:
+    print('nhập vào số dòng muốn đọc:')
+    y = int(input())
+    i= i + 1 
+    
 
-if 1 <= x <=len(lines):
-    print(lines[x-1])
-else:
-    print('file không có dòng này')
+    if 1 <= y <=len(lines):
+       print(lines[y-1])
+    else:
+       print('file không có dòng này')
 f.close()
