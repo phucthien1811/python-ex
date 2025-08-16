740eb23c0e2ab01a4e78f6d3256dd99c3476b8b6
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-16 10:29:50 +0700

    tối ưu không phảđi đoch hêt test 5

--- index.py
+++ index.py
@@ -1,11 +1,17 @@
-f = open("Testfile.txt",'r')
-print('nhập vào dòng muốn đọc: ')
-n = int(input())
-lines = f.readlines()
+def get_line(filename, line_number):
+    with open(filename, "r", encoding="utf-8") as f:
+        for i, line in enumerate(f, start=1):  # enumerate đếm từ 1
+            if i == line_number:
+                return line.strip()   # trả về đúng dòng
+    return None  # nếu không có dòng này
+
+# Nhập số lần cần đọc
+n = int(input("Nhập vào số dòng muốn đọc: "))
+
 for i in range(n):
     y = int(input(f"Nhập vào số thứ tự dòng muốn đọc lần {i+1}: "))
-    if 1 <= y <=len(lines):
-       print(lines[y-1])
+    line = get_line("DanhSachHS.txt", y)
+    if line is not None:
+        print(f"Nội dung dòng {y}: {line}")
     else:
-       print('file không có dòng này')
-f.close()
+        print("File không có dòng này")
