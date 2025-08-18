da66208adacda03b5f1a6351dafa081d8a83acfc
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-18 16:32:16 +0700

    sô đảo ngược bằng chuỗi

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
-n = int(input('nhập vào n giai thừa: '))
+n = input("Nhập số nguyên: ")
+stack = []
 
-if n < 0:
-    print("Giai thừa không xác định cho số âm.")
-else:
-    giai_thua = 1
-    for i in range(1, n + 1):
-        giai_thua *= i
-    print(f"Giai thừa của {n} là: {giai_thua}")
+# Push từng ký tự vào stack
+for ch in n:
+    stack.append(ch)
+
+# Pop ra -> in ngược
+print("Số đảo ngược:", end=" ")
+while stack:
+    print(stack.pop(), end="")
