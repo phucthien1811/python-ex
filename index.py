09291fbf9294c2d993084401d29a7b503dfcace3
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-18 16:36:13 +0700

    chia cho 10 để đảo ngược

--- index.py
+++ index.py
@@ -1,11 +1,21 @@
-n = input("Nhập số nguyên: ")
-stack = []
-
-# Push từng ký tự vào stack
-for ch in n:
-    stack.append(ch)
-
-# Pop ra -> in ngược
-print("Số đảo ngược:", end=" ")
-while stack:
-    print(stack.pop(), end="")
+#Khoi lenh co the phat sinh loi
+try:
+    #Nhap gia tri tu ban phim
+    #Ep kieu du lieu sang so nguyen
+    n = int(input())
+    
+    #Su dung cau truc re nhanh xu ly truong hop n < 0
+    if n < 0:
+        print("Vui long nhap so tu nhien!")
+    else:
+        soDaoNguoc = 0
+        #Su dung vong while de tach cac chu so
+        while n > 0:
+            chuSoCuoi = n % 10
+            soDaoNguoc = soDaoNguoc * 10 + chuSoCuoi
+            n = n // 10
+        print(soDaoNguoc)
+        
+#Khoi lenh duoc thuc thi khi loi xay ra
+except:
+    print("Dinh dang dau vao khong hop le!")
