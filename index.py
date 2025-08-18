c9b39324c4cc7e5b505e1f35c6e39d834a65fc9f
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-18 10:25:00 +0700

    tam giác 2 vòng lặp vd test 1

--- index.py
+++ index.py
@@ -1,30 +1,6 @@
-a, b, c = input('Nhập vào phép tính: ').split()
+n = int(input('nhập vào n: '))
 
-a = float(a)
-c = float(c)
-
-if b == '+':
-    print(a + c)
-elif b == '-':
-    print(a - c)
-elif b == '*':
-    print(a * c)
-elif b == '/':
-    if c != 0:
-        print(a / c)
-    else:
-        print("Lỗi: Không thể chia cho 0")
-elif b == '**':
-    print(a ** c)
-elif b == '%':
-    if c != 0:
-        print(a % c)
-    else:
-        print("Lỗi: Không thể chia cho 0")
-elif b == '//':
-    if c != 0:
-        print(a // c)
-    else:
-        print("Lỗi: Không thể chia cho 0")
-else:
-    print("Phép toán không hợp lệ")
+for i in range(n+1):
+    for j in range(1,i+1):
+        print(j, end=' ')
+    print() 
