103cde45a1dca338ab22bdc302f02b6a2d7b6d63
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-18 11:40:29 +0700

    giai thừa n

--- index.py
+++ index.py
@@ -1,32 +1,9 @@
-#Khoi lenh co the phat sinh loi
-try:
-    #Nhap gia tri tu ban phim
-    #Ep kieu du lieu sang so nguyen
-    n = int(input())
-    
-    #Su dung cau truc re nhanh xu ly truong hop n < 0
-    if n<0:
-        print("Vui long nhap so tu nhien!")
-    else: 
-        #Ma ascii cua ky tu 'A'
-        maAscii = 65
-        #Su dung vong lap for de suyet cac dong
-        for i in range(n):
-            #Tinh toan khoang trang dau moi dong
-            khoangTrangDauDong = " "*((n-i)*2-1)
-            print(khoangTrangDauDong, end='')
-            #Su dung vong lap for de in cac ky tu tren mot dong
-            for j in range(2*i+1):
-                #Chuyen tu ma ascii sang ky tu
-                chuCai = chr(maAscii)
-                print(chuCai, end=" ")
-                #Tang ma ascii len 1 de duyet ky tu tiep theo
-                maAscii += 1
-                #Neu ky tu vuot qua chu Z thi quay lai ky tu A ban dau
-                if chr(maAscii) > 'Z':
-                    maAscii = 65
-            print()
+n = int(input('nhập vào n giai thừa: '))
 
-#Khoi lenh duoc thuc thi khi loi xay ra
-except:
-    print("Dinh dang dau vao khong hop le!")
+if n < 0:
+    print("Giai thừa không xác định cho số âm.")
+else:
+    giai_thua = 1
+    for i in range(1, n + 1):
+        giai_thua *= i
+    print(f"Giai thừa của {n} là: {giai_thua}")
