7d4f78ee123eed8580a7ce411655c75111c87a60
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-20 15:07:49 +0700

    fibonaci

--- index.py
+++ index.py
@@ -1,27 +1,22 @@
 #Khoi lenh co the phat sinh loi
 try:
-    #Nhap gia tri tu ban phim
-    #Ep kieu du lieu sang so nguyen
-    n = int(input())
-    
-    #Su dung cau truc re nhanh xu ly truong hop n < 0
-    if n < 0:
-        print("Vui long nhap so tu nhien!")
-    else:
-        tongChuSoChan = 0
-        tongChuSoLe = 0
-        #Su dung vong while de tach cac chu so
-        while n > 0:
-            #Kiem tra chu so cuoi la chan hay le
-            if n % 2 == 0:
-                tongChuSoChan += n % 10
-            else:
-                tongChuSoLe += n % 10
-            n = n // 10
-
-        ketQua = tongChuSoChan * tongChuSoLe
-        print(ketQua)
-        
+   #Nhap gia tri tu ban phim
+   #Ep kieu du lieu sang so nguyen
+   n = int(input())
+  
+   #Su dung cau truc re nhanh xu ly truong hop n < 0
+   if n <= 0:
+       print("Vui long nhap so nguyen duong!")
+   elif n == 1 or n == 2:
+       print(1)
+   else:
+       soThuNhat, soThuHai = 1, 1
+       for i in range(n-2):
+           #soThuNhat (moi) = soThuHai (cu)
+           #soThuHai (moi) = soThuNhat (cu) + soThuHai (cu)
+           soThuNhat, soThuHai = soThuHai, soThuNhat + soThuHai
+       print(soThuHai)
+      
 #Khoi lenh duoc thuc thi khi loi xay ra
 except:
-    print("Dinh dang dau vao khong hop le!")
+   print("Dinh dang dau vao khong hop le!")
