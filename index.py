8fbacbd7426db8362e804cf6ea6cc7e637b2ca87
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-20 15:27:58 +0700

    hiển thị tất cả các ước

--- index.py
+++ index.py
@@ -1,22 +1,5 @@
-#Khoi lenh co the phat sinh loi
-try:
-   #Nhap gia tri tu ban phim
-   #Ep kieu du lieu sang so nguyen
-   n = int(input())
-  
-   #Su dung cau truc re nhanh xu ly truong hop n < 0
-   if n <= 0:
-       print("Vui long nhap so nguyen duong!")
-   elif n == 1 or n == 2:
-       print(1)
-   else:
-       soThuNhat, soThuHai = 1, 1
-       for i in range(n-2):
-           #soThuNhat (moi) = soThuHai (cu)
-           #soThuHai (moi) = soThuNhat (cu) + soThuHai (cu)
-           soThuNhat, soThuHai = soThuHai, soThuNhat + soThuHai
-       print(soThuHai)
-      
-#Khoi lenh duoc thuc thi khi loi xay ra
-except:
-   print("Dinh dang dau vao khong hop le!")
+n = int(input('nhập vào n: '))
+
+for i  in range(1, n + 1):
+    if n % i == 0:
+        print(i, end=' ')
