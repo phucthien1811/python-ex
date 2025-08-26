5988452f144af3167722a06d563bc7daf861aaa3
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-26 15:45:37 +0700

    đoạn a b tìm tất cả số nguyên tố

--- index.py
+++ index.py
@@ -1,20 +1,16 @@
-def is_prime(num):
-    if num <= 1:
-        return False
-    if num == 2:     # 2 là số nguyên tố duy nhất chẵn
-        return True
-    if num % 2 == 0: # loại bỏ các số chẵn > 2
-        return False
-    for i in range(3, int(num**0.5) + 1, 2):  # duyệt số lẻ
-        if num % i == 0:
-            return False
-    return True
-
-# Nhập từ bàn phím
-n = int(input("Nhập vào n: "))
-
-# Kiểm tra và in kết quả
-if is_prime(n):
-    print(f"{n} là số nguyên tố")
-else:
-    print(f"{n} không phải là số nguyên tố")
+a = int(input('nhaap vao a: '))
+b= int (input('nhap vao b: '))
+if a < 2:
+    print('khong co so nguyen to')
+if  a == 2:
+    print('2 la so nguyen to')
+for i in range(3,b+1):
+    if i % 2 == 0:
+        continue
+    elif i > 2 :
+        for j in range(3,i,2):
+            if i % j == 0:
+                break
+        else:
+            if i >= a:
+                print(i,'la so nguyen to')
