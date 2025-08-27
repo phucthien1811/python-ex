f516f31d74c069dd1617d7d9596fb7e476021fac
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-27 08:34:58 +0700

    hàm sử dụng máy tính

--- index.py
+++ index.py
@@ -1,13 +1,45 @@
-def tinh_tong(n):
-   if n:
-       return n + tinh_tong(n - 1)
-   return 0
+#Dinh nghia ham
+def cong(soHangThuNhat, soHangThuHai):
+   return soHangThuNhat + soHangThuHai
+
+def tru(soBiTru, soTru):
+   return soBiTru - soTru
+
+def nhan(thuaSoThuNhat, thuaSoThuHai):
+   return thuaSoThuNhat * thuaSoThuHai
+
+def chia(soBiChia, soChia):
+   if soChia == 0:
+       return "ChiaCho0"
+   return soBiChia / soChia
+
+def may_tinh(soThuNhat, soThuHai, phepTinh):
+   if phepTinh == '+':
+       return cong(soThuNhat, soThuHai)
+   if phepTinh == '-':
+       return tru(soThuNhat, soThuHai)
+   if phepTinh == 'x':
+       return nhan(soThuNhat, soThuHai)
+   if phepTinh == ':':
+       return chia(soThuNhat, soThuHai)
+
+#Khoi lenh co the phat sinh loi
 try:
-    n = int(input("nhập số nguyên dương n:"))
-    if n < 0:
-        print("vui lòng nhập số nguyên dương")
-    else:
-        print(f"Tổng các số từ 1 đến {n} là: {tinh_tong(n)}")
-except ValueError:
-    print("vui lòng nhập số nguyên hợp lệ")
-        
+   #Nhap bieu thuc tu ban phim
+   soThu1, phepTinh, soThu2 = input().split()
+  
+   #Ep kieu du lieu sang so thuc
+   soThu1 = float(soThu1)
+   soThu2 = float(soThu2)
+
+   #Goi thuc thi ham va truyen cac tham so cho ham
+   ketQua = may_tinh(soThu1, soThu2, phepTinh)
+
+   if ketQua == "ChiaCho0":
+       print("So chia phai khac 0!")
+   else:
+       #In ra man hinh bieu thuc va ket qua
+       print("{} {} {} = {}".format(soThu1, phepTinh, soThu2, ketQua))   
+#Khoi lenh duoc thuc thi khi loi xay ra
+except:
+   print("Dinh dang dau vao khong hop le!")
