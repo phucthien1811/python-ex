8240731a73975bd0753f787b2d9e999fa2374c1d
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-27 08:12:12 +0700

    tổng n bằng đệ quy

--- index.py
+++ index.py
@@ -1,8 +1,13 @@
-def tinh_tong(*args):
-   tong = 0
-   for so in args:
-       tong += so
-   return tong
-
-print("1 + 2 + 3 = {}".format(tinh_tong(1, 2, 3)))
-print("1 + 2 + 3 + 4 + 5 = {}".format(tinh_tong(1, 2, 3, 4, 5)))
+def tinh_tong(n):
+   if n:
+       return n + tinh_tong(n - 1)
+   return 0
+try:
+    n = int(input("nhập số nguyên dương n:"))
+    if n < 0:
+        print("vui lòng nhập số nguyên dương")
+    else:
+        print(f"Tổng các số từ 1 đến {n} là: {tinh_tong(n)}")
+except ValueError:
+    print("vui lòng nhập số nguyên hợp lệ")
+        
