c80ddeb29d96eab8b9c21cdf5100e870f30aa485
Thien Tiy <jack0baohbocon@gmail.com>
2025-09-04 15:38:57 +0700

    hàm số hoàn thiện

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
-def tinhgiaithua(n):
-    if n==0 or n ==1:
-        return 1
-    return n * tinhgiaithua(n-1)
-         
-   
+def sohoanthien(n):
+    tong = 0
+    for i in range(1, n):
+        if n % i == 0:
+            tong+=i
+    if tong == n:
+        return "{} là số hoàn thiện ".format(n)
+    else :
+        return "{} không phải là số hoàn thiện".format(n)
 
 
-a = int(input('nhap vao n: '))
+n = int(input('Nhập vào số N: '))
 
-ketqua = tinhgiaithua(a)
+ketqua = sohoanthien(n)
 print(ketqua)
- 
