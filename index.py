c808b480f5fed1927f0d2718058d0311811e9bb1
Thien Tiy <jack0baohbocon@gmail.com>
2025-09-04 15:47:50 +0700

    howk team cách in ra các số hoàn thiện

--- index.py
+++ index.py
@@ -1,14 +1,31 @@
-def liet_ke_so_hoan_thien(n):
-    ket_qua = []
-    for num in range(2, n+1):   # duyệt từ 2 đến n
-        tong = 0
-        for i in range(1, num):
-            if num % i == 0:
-                tong += i
-        if tong == num:
-            ket_qua.append(num)
-    return ket_qua
+def kiem_tra_so_hoan_thien(n):
+   tongCacUoc = 0
+   #Su dung vong lap for de duyet cac so tu 1 den n // 2 + 1
+   for i in range(1, n // 2 + 1):
+       #Kiem tra tinh chia het
+       if n % i == 0:
+           #Tinh tong cac uoc
+           tongCacUoc += i
+   if n == tongCacUoc:
+       return True
+   return False
 
-# Ví dụ chạy
-n = 10000
-print(f"Các số hoàn thiện <= {n}: {liet_ke_so_hoan_thien(n)}")
+def liet_ke_so_hoan_thien(n):
+   for i in range(1, n):
+       if kiem_tra_so_hoan_thien(i):
+           print(i, end=' ')
+
+#Khoi lenh co the phat sinh loi
+try:
+   #Nhap gia tri tu ban phim
+   #Ep kieu du lieu sang so nguyen
+   n = int(input())
+  
+   #Su dung cau truc re nhanh xu ly truong hop n < 0
+   if n < 0:
+       print("Vui long nhap so tu nhien!")
+   else:
+       liet_ke_so_hoan_thien(n)
+#Khoi lenh duoc thuc thi khi loi xay ra
+except:
+   print("Dinh dang dau vao khong hop le!")
